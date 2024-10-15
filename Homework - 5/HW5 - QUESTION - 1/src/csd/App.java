6c04aa1dc46bdcbbdb8796b96abc15c8f3758ab6
hyenicil <57847728+hyenicil@users.noreply.github.com>
2024-10-15 21:59:27 +0300

    Burada ornekle beraber cozuldu.

---
+++ Homework - 5/HW5 - QUESTION - 1/src/csd/App.java
@@ -0,0 +1,33 @@
+package csd;
+
+class App {
+    public static void main(String[] args) {
+        NumberUtilTest.run();
+    }
+}
+class NumberUtilTest {
+    public static void run() {
+        System.out.println(NumberUtil.series(10));
+    }
+}
+
+class NumberUtil {
+
+    public static int factorial (int n) {
+        int result = 1;
+        for (int i = 2;i <=n; ++i) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static double series(int n) {
+        double total = 1;
+        for(int i = 1; i <= n; i++) {
+            total = total + ( 1. / factorial(i));
+        }
+        return total;
+
+    }
+
+}
