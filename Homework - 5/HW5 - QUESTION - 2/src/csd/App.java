53af490e52ba496cc25aa5bda3abd8697fce7bf1
hyenicil <57847728+hyenicil@users.noreply.github.com>
2024-10-17 00:04:37 +0300

    Burada bir daha bakmak gerek

--- Homework - 5/HW5 - QUESTION - 2/src/csd/App.java
+++ Homework - 5/HW5 - QUESTION - 2/src/csd/App.java
@@ -1,12 +1,12 @@
 package csd;
 //Burayi yaparken zorlandim ve daha sonra cozumlere bakilacak.
 /*
-*
-* Parametresi ile aldığı pozitif bir tamsayının kendisi hariç tüm çarpanlarının toplamı ile geri dönen sumFactors
-* metodunu yazınız. Metot parametresi ile aldığı sayı hariç tüm çarpanlarının toplamına geri dönecektir: (1 her
-* sayının doğal çarpanıdır ve toplama dahil edilecektir.)
-*
-*  */
+ *
+ * Parametresi ile aldığı pozitif bir tamsayının kendisi hariç tüm çarpanlarının toplamı ile geri dönen sumFactors
+ * metodunu yazınız. Metot parametresi ile aldığı sayı hariç tüm çarpanlarının toplamına geri dönecektir: (1 her
+ * sayının doğal çarpanıdır ve toplama dahil edilecektir.)
+ *
+ *  */
 class App {
     public static void main(String[] args) {
         NumberUtilTest.run();
@@ -37,9 +37,9 @@ class NumberUtil {
     public static long sumFactors(long val) {
         long result = 1;
 
-            for (long i = 2; i * i <= val; ++i)
-                if (val % i == 0)
-                    result += (i == val/i) ? i : ( i + val/i);
+        for (long i = 2; i * i <= val; ++i)
+            if (val % i == 0)
+                result += (i == val/i) ? i : ( i + val/i);
         return result;
     }
 }
