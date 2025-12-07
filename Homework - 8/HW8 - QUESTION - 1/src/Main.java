edf7ab13112621d6dbd4bd7920a2dfa702caea86
hyenicil <57847728+hyenicil@users.noreply.github.com>
2025-12-07 13:45:47 +0300

    [HW8-Q1] The answer to the question has been completed.

---
+++ Homework - 8/HW8 - QUESTION - 1/src/Main.java
@@ -0,0 +1,40 @@
+/*
+* Klavyeden bir yazı giriniz. Yazı içerisinde iç içe küme parantezleri bulunuyor olsun. En içteki küme parantezinin içerisindeki yazıyı yazdırınız.
+* Örneğin:
+* Yazı: Bu örnek {aslında {hiç zor}} değil
+* Sonuç: hiç zor
+* Yazı: Bu örnekten {daha {zor {örnekler de yapacağız} değil}} mi?
+* Sonuç: örnekler de yapacağız
+* Yazı: Yanlış {bir giriş için hata mesajı yazdırılacak
+* Sonuç: yanlış bir giriş
+* */
+public class Main {
+    public static void main(String[] args) {
+        Test.run();
+    }
+}
+
+class Test {
+    public static void run() {
+        ParserBracketsUtil.indexBrackets("Yanlış {bir giriş için hata mesajı yazdırılacak.");
+    }
+}
+
+class ParserBracketsUtil {
+    public static final String OPEN = "{";
+    public static final String CLOSE = "}";
+
+    public static void errorMessage (String text) {
+        System.out.printf("%s\n",text);
+    }
+    public static void indexBrackets(String input) {
+        int firstClosedBracket = input.indexOf(CLOSE);
+
+        if (firstClosedBracket == -1) {
+           errorMessage("Sonuç: yanlış bir giriş");
+        }
+        else {
+            System.out.println(input.substring(input.lastIndexOf(OPEN, firstClosedBracket)+1, firstClosedBracket));
+        }
+    }
+}
